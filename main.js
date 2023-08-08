177a8bbb80564465cd7ec871702fb53a9f22b5dc
Kimutai <jankimsingoei@gmail.com>
2023-08-08 13:27:59 +0300

    done

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let hidden = document.querySelector('#modal')
+let errorMessage = document.getElementById('modal-message');
+
+hidden.className= "hidden";
+
+document.addEventListener('DOMContentLoaded',()=>{
+  let heart = document.querySelectorAll('.like-glyph');
+  heart.forEach(item => item.addEventListener('click', (e) => {
+    e.preventDefault();
+    mimicServerCall() 
+
+
+
+
 
 
 
@@ -17,9 +31,18 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
+        item.classList.remove('activated-heart')
+        alert('remove')
+
       } else {
         resolve("Pretend remote server notified of action!");
+        
+        item.classList.add('activated-heart')
+        alert('add')
+        
       }
     }, 300);
   });
 }
+}))
+})
